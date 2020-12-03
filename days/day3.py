270c014d3b830f7c5ca5b8fa70caf475d82e11dd
Olivia Palmu <oliviaspalmu@gmail.com>
2020-12-03 14:35:46 +0900

    Day 3 naive

---
+++ days/day3.py
@@ -0,0 +1,35 @@
+import itertools
+
+def solve_naive(data):
+    first = 0
+    width = len(data[0])
+    i = 0
+    for row in data[1:]:
+        i += 3
+        if row[i % width] == "#":
+            first += 1
+    second = 0
+    i=j=k=l=m = 0
+    acc = [0,0,0,0,0]
+    for row in data[1:]:
+        i += 1
+        j += 3
+        k += 5
+        l += 7
+        if row[i % width] == "#":
+            acc[0] += 1
+        if row[j % width] == "#":
+            acc[1] += 1
+        if row[k % width] == "#":
+            acc[2] += 1
+        if row[l % width] == "#":
+            acc[3] += 1
+        if i % 2 == 0:
+            m += 1
+            if row[m % width] == "#":
+                acc[4] += 1
+    print(acc)
+    second = acc[0] * acc[1] * acc[2] * acc[3] * acc[4]
+    return [first, second]
+
+implementations = (solve_naive,)
