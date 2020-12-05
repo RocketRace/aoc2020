748caf26a8d3502c5ed7de9a2e6119363046511f
Olivia Palmu <oliviaspalmu@gmail.com>
2020-12-05 15:01:50 +0900

    Better and more correct day 5

--- days/day5.py
+++ days/day5.py
@@ -6,10 +6,25 @@ def solve_naive(data: List[str]):
         for line in data
     ]
     first = max(data)
-    data = {x for x in data if 7 < x < 1016}
-    comp = {a * 8 + b for a in range(8, 113) for b in range(0, 8)}
+    data = {x for x in data if 7 < x < 1016} # Uh oh read the question wrong
+    comp = {a * 8 + b for a in range(8, 113) for b in range(0, 8)} # uh oh hardcoded
     second = comp.difference(data).pop()
 
     return [first, second]
 
-implementations = (solve_naive,)
+def solve_less_naive(data: List[str]):
+    data = [
+        int(x.replace("F","0").replace("L","0").replace("B","1").replace("R","1"), base=2)
+        for x in data
+    ]
+    first = max(data)
+    second = 0
+    comp = set(data)
+    for i in range(min(data), first + 1):
+        if i not in comp:
+            second = i
+            break
+
+    return [first, second]
+
+implementations = (solve_naive, solve_less_naive,)
