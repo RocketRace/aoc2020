d7d0b6b669f7c774d2efa4fff2c6405aa80e7355
Olivia Palmu <oliviaspalmu@gmail.com>
2020-12-10 15:23:56 +0900

    add pretty colors

--- aoc.py
+++ aoc.py
@@ -12,26 +12,31 @@ def check(day):
         exec(fp.read(), new_globals)
         impls = new_globals["implementations"]
         if len(impls) == 1:
-            print(f"Day {day}: 1 implementation found.")
+            print(f"\033[1mDay {day}: 1 implementation found.\033[0m")
         else:
-            print(f"Day {day}: {len(impls)} implementations found.")
+            print(f"\033[1mDay {day}: {len(impls)} implementations found.\033[0m")
+        s_1, s_2 = solutions[day - 1]
         for i, impl in enumerate(impls):
             t = time()
-            solution = impl(data)
+            p_1, p_2 = impl(data)
             dt = time() - t
-            success = solution == solutions.get(str(day))
-            count = int(0.1 / dt)
+            count = int(0.5 / dt) + 1
             total = 0
             for _ in range(count):
                 t = time()
-                solution = impl(data)
+                impl(data)
                 dt = time() - t
                 total += dt
-            mean = total + count + 0.1
-            if success:
-                print(f"{i + 1}. Passed in {mean:.3} s: {solution}")
+            mean = total / count
+            print(f"Implementation {i + 1} took {mean:.3} s:")
+            if p_1 == s_1:
+                print(f"- Part 1 \033[32;40;1mpassed\033[0m (got {p_1})")
             else:
-                print(f"{i + 1}. Failed in {mean:.3} s: Expected {solutions.get(str(day))}, got {solution}")
+                print(f"- Part 1 \033[31;40;1mfailed\033[0m (expected {s_1}, got {p_1})")
+            if p_2 == s_2:
+                print(f"- Part 2 \033[32;40;1mpassed\033[0m (got {p_2})")
+            else:
+                print(f"- Part 2 \033[31;40;1mfailed\033[0m (expected {s_2}, got {p_2})")
 
 day = input("Day to solve (omit to solve all): ").strip()
 
@@ -39,7 +44,7 @@ if not day:
     try:
         for i in range(25):
             check(i + 1)
-    except:
+    except FileNotFoundError:
         print("No more days found.")
 else:
-    check(day)
+    check(int(day))
