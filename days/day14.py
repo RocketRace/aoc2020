ca0d90f8be1471cb549af45225d7c03796006b7d
Olivia Palmu <oliviaspalmu@gmail.com>
2020-12-14 14:59:28 +0900

    Python makes me REALLY want to use strings for bit magic because it's easier

---
+++ days/day14.py
@@ -0,0 +1,35 @@
+from typing import List
+import itertools
+def solve_naive(data: List[str]):
+    ones = zeros = 0
+    mem = {}
+    for line in data:
+        if line.startswith("mask ="):
+            mask = line[6:]
+            ones = int("".join("1" if x == "1" else "0" for x in mask), base=2)
+            zeros = int("".join("1" if x == "0" else "0" for x in mask), base=2)
+        else:
+            addr = line[4:line.find("]")]
+            num = int(line[line.find("=")+2:])
+            mem[addr] =( num | ones) & ~ zeros
+    first = sum([*mem.values()])
+    ones = 0
+    positions = []
+    mem = {}
+    for line in data:
+        if line.startswith("mask ="):
+            mask = line[6:]
+            ones = int("".join("1" if x == "1" else "0" for x in mask), base=2)
+            positions = [36 - i for i, x in enumerate(mask) if x == "X"]
+        else:
+            addr = int(line[4:line.find("]")])
+            addr |= ones
+            for i in range(1 << len(positions)):
+                n = list(bin(addr)[2:].rjust(36, "0"))
+                for bit, pos in zip(bin(i)[2:].rjust(len(positions), "0"), positions):
+                    n[35-pos] = bit
+                mem["".join(reversed(n))] = int(line[line.find("=")+2:])
+    second = sum([*mem.values()])
+    return [first, second]
+
+implementations = (solve_naive,)
