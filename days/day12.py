9c0b550b75a6dac3dd746825cecbcf58d1ddc38d
Olivia Palmu <oliviaspalmu@gmail.com>
2020-12-14 11:22:43 +0900

    225 bytes

--- days/day12.py
+++ days/day12.py
@@ -57,8 +57,8 @@ def solve_naive(data: List[str]):
     return [first, second]
 
 def s(I):
- f=lambda c:abs(c.real)+abs(c.imag);p=P=0;d=1;w=10-1j;g=[1j,-1j,1]
- for i,*r in I:a=int(''.join(r));x=(g+[-1,0])["SNEW".find(i)]*a;y=g["RL".find(i)]**(a//90);z=a*(i=="F");p+=x+z*d;P+=z*w;w=w*y+x;d*=y
+ f=lambda c:abs(c.real)+abs(c.imag);p=P=0;d=1;w=10-1j;g=1j,-1j,1
+ for i,*r in I:a=int(''.join(r));x=(g+(-1,0))["SNEW".find(i)]*a;y=g["RL".find(i)]**(a//90);z=a*(i=="F");p+=x+z*d;P+=z*w;w=w*y+x;d*=y
  return f(p),f(P)
 
 implementations = (solve_naive,s)
