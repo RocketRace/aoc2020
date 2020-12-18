d2b18484e36a06177e8ee8c9edaa3e1c1cbaa6c9
Olivia Palmu <oliviaspalmu@gmail.com>
2020-12-18 10:19:14 +0900

    generalize day 17 to N dimensions because why not lol

--- days/day17.py
+++ days/day17.py
@@ -1,93 +1,101 @@
 from typing import List
 import itertools
 def solve_naive(data: List[str]):
-    first = 0
     # set containing all active cells (x, y, z)
     # because each dimension can be infinite (i.e. lists are inconvenient)
-    actives = set()
-    actives_4 = set()
+    actives_3d = set()
+    actives_4d = set()
+    actives_10d = set()
     for y, row in enumerate(data):
         for x, bit in enumerate(row):
             if bit == "#":
-                actives.add((x, y, 0))
-                actives_4.add((x, y, 0, 4))
+                actives_3d.add((x, y, 0))
+                actives_4d.add((x, y, 0, 0))
+                actives_10d.add((x, y, 0, 0, 0, 0, 0, 0, 0, 0))
 
-    deltas_3 = [x for x in itertools.product((0,1,-1), repeat=3) if x != (0,0,0)]
-    def get_counts_3(x, y, z):
-        count = 0
-        for dx, dy, dz in deltas_3:
-            new_pos = (x + dx, y + dy, z + dz)
-            if new_pos in actives:
-                count += 1
-        return count
-    
-    def add_empties(x, y, z, *, empties: set):
-        count = 0
-        i = 0
-        for dx, dy, dz in deltas_3:
-            i += 1
-            new_pos = (x + dx, y + dy, z + dz)
-            if new_pos in actives:
-                count += 1
-            else:
-                empties.add(new_pos)
-        return count
+    def solve_dimensions(initial, *, n, t):
+        actives = initial
+        zero = tuple([0 for _ in range(n)])
+        deltas = [x for x in itertools.product((0, 1, -1), repeat=n) if x != zero]
+        
+        if n == 3:
+            def get_counts(pos):
+                count = 0
+                x, y, z = pos
+                for dx, dy, dz in deltas:
+                    new_pos = (x + dx, y + dy, z + dz)
+                    if new_pos in actives:
+                        count += 1
+                return count
+        elif n == 4:
+            def get_counts(pos):
+                count = 0
+                x, y, z, w = pos
+                for dx, dy, dz, dw in deltas:
+                    new_pos = (x + dx, y + dy, z + dz, w + dw)
+                    if new_pos in actives:
+                        count += 1
+                return count
+        else:
+            def get_counts(pos):
+                count = 0
+                for delta_pos in deltas:
+                    new_pos = tuple([x + y for x, y in zip(pos, delta_pos)])
+                    if new_pos in actives:
+                        count += 1
+                return count
+        if n == 3:
+            def add_empties(pos, empties):
+                count = 0
+                x, y, z = pos
+                for dx, dy, dz in deltas:
+                    new_pos = (x + dx, y + dy, z + dz)
+                    if new_pos in actives:
+                        count += 1
+                    else:
+                        empties.add(new_pos)
+                return count
+        elif n == 4:
+            def add_empties(pos, empties):
+                count = 0
+                x, y, z, w = pos
+                for dx, dy, dz, dw in deltas:
+                    new_pos = (x + dx, y + dy, z + dz, w + dw)
+                    if new_pos in actives:
+                        count += 1
+                    else:
+                        empties.add(new_pos)
+                return count
+        else:
+            def add_empties(pos, empties):
+                count = 0
+                for delta_pos in deltas:
+                    new_pos = tuple([x + y for x, y in zip(pos, delta_pos)])
+                    if new_pos in actives:
+                        count += 1
+                    else:
+                        empties.add(new_pos)
+                return count
 
-    for _ in range(6):
-        empties = set()
-        remove = set()
-        new = set()
-        for pos in actives:
-            count = add_empties(*pos, empties = empties)
-            if count != 2 and count != 3:
-                remove.add(pos)
-        for pos in empties:
-            count = get_counts_3(*pos)
-            if count == 3:
-                new.add(pos)
-        actives.update(new)
-        actives.difference_update(remove)
+        for _ in range(t):
+            empties = set()
+            remove = set()
+            new = set()
+            for pos in actives:
+                count = add_empties(pos, empties)
+                if count != 2 and count != 3:
+                    remove.add(pos)
+            for pos in empties:
+                count = get_counts(pos)
+                if count == 3:
+                    new.add(pos)
+            actives |= new
+            actives -= remove
+        
+        return len(actives)
 
-    first = len(actives)
-
-    deltas_4 = [x for x in itertools.product((0,1,-1), repeat=4) if x != (0,0,0,0)]
-    def get_counts_4(x, y, z, w):
-        count = 0
-        for dx, dy, dz, dw in deltas_4:
-            new_pos = (x + dx, y + dy, z + dz, w + dw)
-            if new_pos in actives_4:
-                count += 1
-        return count
-    
-    def add_empties_4(x, y, z, w, *, empties: set):
-        count = 0
-        i = 0
-        for dx, dy, dz, dw in deltas_4:
-            i += 1
-            new_pos = (x + dx, y + dy, z + dz, w + dw)
-            if new_pos in actives_4:
-                count += 1
-            else:
-                empties.add(new_pos)
-        return count
-
-    for _ in range(6):
-        empties = set()
-        remove = set()
-        new = set()
-        for pos in actives_4:
-            count = add_empties_4(*pos, empties = empties)
-            if count != 2 and count != 3:
-                remove.add(pos)
-        for pos in empties:
-            count = get_counts_4(*pos)
-            if count == 3:
-                new.add(pos)
-        actives_4.update(new)
-        actives_4.difference_update(remove)
-
-
-    second = len(actives_4)
+    first = solve_dimensions(actives_3d, n=3, t=6)
+    second = solve_dimensions(actives_4d, n=4, t=6)
     return [first, second]
 
 implementations = (solve_naive,)
